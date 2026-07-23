ee847aec3800a8ae1dfe23b9aebc3be8dc7730f9
veil-kikaku <neetland@gmail.com>
2026-07-23 13:15:04 +0900

    202607231315

--- gift-map.js
+++ gift-map.js
@@ -11,12 +11,102 @@ const GIFT_MAP = {
     // ----------------------------
 
     tea_money: {
-        name: "Tea money",
+        name: "お茶代",
         jewel: 90
     },
 
     laughing_bear_www: {
         name: "wwwww",
+        jewel: 320
+    },
+
+    ramen_jp: {
+        name: "ラーメン",
+        jewel: 100
+    },
+
+    w_key: {
+        name: "w",
+        jewel: 10
+    },
+
+    smile: {
+        name: ":D",
+        jewel: 5
+    },
+
+    bow: {
+        name: "一礼",
+        jewel: 3
+    },
+
+    v_sign: {
+        name: "ピース",
+        jewel: 2
+    },
+
+    heart: {
+        name: "ハート",
+        jewel: 10
+    },
+
+    corgi: {
+        name: "コーギー",
+        jewel: 1000
+    },
+
+    chili_v2_320x320: {
+        name: "唐辛子",
+        jewel: 6
+    },
+
+    shaved_ice: {
+        name: "かき氷",
+        jewel: 250
+    },
+
+    cream_soda: {
+        name: "クリームソーダ",
+        jewel: 250
+    },
+
+    donut: {
+        name: "ドーナツ",
+        jewel: 30
+    },
+
+    waiting: {
+        name: "待ってます",
+        jewel: 120
+    },
+
+    power_potion: {
+        name: "パワーポーション",
+        jewel: 500
+    },
+
+    numaru: {
+        name: "沼る",
+        jewel: 120
+    },
+
+    press_f: {
+        name: "Fキーを押す",
+        jewel: 10
+    },
+
+    yoshiyoshi: {
+        name: "よしよし",
+        jewel: 120
+    },
+
+    kanpai: {
+        name: "かんぱい",
+        jewel: 320
+    },
+
+    xxxxx: {
+        name: "xxxx",
         jewel: 0
     }
 
